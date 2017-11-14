749276acc7e92bab84d5f31d5c04f1ef5db49251
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-11-14 15:59:40 +0100

    feat: add redux-logger

--- src/panel/store/configureStore.js
+++ src/panel/store/configureStore.js
@@ -1,8 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
 const configureStore = () => {
-  return createStore(rootReducer);
+  return createStore(
+    rootReducer,
+    applyMiddleware(logger),
+  );
 };
 
 export default configureStore;
