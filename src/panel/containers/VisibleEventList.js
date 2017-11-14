17b6aaddbd019f4d1c9f2e66bafe2c116779e0e8
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-11-14 16:56:33 +0100

    feat(VisibleEventList): bind click to Event

--- src/panel/containers/VisibleEventList.js
+++ src/panel/containers/VisibleEventList.js
@@ -2,31 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getEvents } from '../modules/events/reducer';
+import { selectEvent } from '../modules/events/actions';
 import EventList from '../components/EventList';
 import Event from '../components/Event';
 
-const VisibleEventList = ({ messages }) => (
+const VisibleEventList = ({ events, onEventClick }) => (
   <EventList>
-    {messages.map(({ id, type, time }) => (
+    {events.map(({ id, type, time }) => (
       <Event
         key={id}
         type={type}
         time={time}
+        onClick={onEventClick(id)}
       />
     ))}
   </EventList>
 );
 
 VisibleEventList.propTypes = {
-  messages: PropTypes.arrayOf(PropTypes.shape({
+  events: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     time: PropTypes.number.isRequired,
   }).isRequired).isRequired,
+  onEventClick: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
-  messages: getEvents(state),
+  events: getEvents(state),
 });
 
-export default connect(mapStateToProps)(VisibleEventList);
+const mapDispatchToProps = dispatch => ({
+  onEventClick: id => () => dispatch(selectEvent(id)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(VisibleEventList);
