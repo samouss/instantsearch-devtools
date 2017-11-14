ef1cd618db019299b63d1d643e53ebabb796909a
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-11-14 13:17:22 +0100

    feat(Tag): add component

---
+++ src/panel/components/Tag/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './index.css';
+
+const Tag = ({ type }) => (
+  <span styleName={`Tag Tag--${type.toLowerCase()}`}>
+    {type.toUpperCase()}
+  </span>
+);
+
+Tag.propTypes = {
+  type: PropTypes.string.isRequired,
+};
+
+export default Tag;
