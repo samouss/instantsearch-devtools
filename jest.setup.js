1ae33907f250ecd7df5e4ab60621a3da9c6439fa
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-12-16 14:46:51 +0100

    chore: move babel/polyfill from setup to polyfill

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,5 @@
 /* eslint "import/no-extraneous-dependencies": ["error", { "devDependencies": true }] */
 
-import '@babel/polyfill';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
