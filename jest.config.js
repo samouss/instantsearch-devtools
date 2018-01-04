1afdd90977ce821e04dcf45b2d6154948f1a9e39
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-01-04 19:28:05 +0100

    chore: support alias for test

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   moduleDirectories: ['src', 'node_modules'],
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
+    '^test/(.*)$': '<rootDir>/test/$1',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   setupFiles: ['<rootDir>/jest.polyfills.js', '<rootDir>/jest.setup.js'],
