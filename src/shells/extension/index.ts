8d5fb2a0dc202fe24dd11c35285f21ab1f0a7de6
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-02-02 18:05:16 +0100

    fix(extension): create application adapter on earch refresh

--- src/shells/extension/index.ts
+++ src/shells/extension/index.ts
@@ -2,15 +2,14 @@ import createApplicationBridge from '../../bridge/createApplicationBridge';
 import createExtensionApplicationAdapter from './createExtensionApplicationAdapter';
 
 const container = document.getElementById('root') as HTMLElement;
-const adapter = createExtensionApplicationAdapter();
 
 // Create the App on each ⌘ + R
 chrome.devtools.network.onNavigated.addListener(() => {
-  createApplicationBridge(adapter, {
+  createApplicationBridge(createExtensionApplicationAdapter(), {
     container,
   });
 });
 
-createApplicationBridge(adapter, {
+createApplicationBridge(createExtensionApplicationAdapter(), {
   container,
 });
