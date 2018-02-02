6217ef07f418739edd0ed0da554cb256502c5fbb
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-02-02 18:09:23 +0100

    refacator(extension): rename hook for inject

--- src/shells/extension/injectLoader/injectLoaderDevelopment.ts
+++ src/shells/extension/injectLoader/injectLoaderDevelopment.ts
@@ -1,5 +1,5 @@
 const scriptAsync = document.createElement('script');
-scriptAsync.src = chrome.extension.getURL('hook.js');
+scriptAsync.src = chrome.extension.getURL('inject.js');
 scriptAsync.onload = () => {
   window.postMessage({ source: 'chrome-devtools-experiments-loader' }, '*');
   scriptAsync.remove();
--- src/shells/extension/injectLoader/injectLoaderProduction.ts
+++ src/shells/extension/injectLoader/injectLoaderProduction.ts
@@ -1,5 +1,5 @@
 const scriptSync = document.createElement('script');
 // tslint:disable-next-line no-var-requires
-scriptSync.textContent = require('!raw-loader!../../dist/hook.js');
+scriptSync.textContent = require('!raw-loader!../../../../dist/inject.js');
 document.documentElement.appendChild(scriptSync);
 scriptSync.remove();
