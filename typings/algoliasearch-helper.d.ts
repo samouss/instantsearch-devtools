db0e5e9c72a7dd46408335e154568889e88956d1
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-02-02 18:35:03 +0100

    chore: add fake typings definition for alogliasearch / helper

---
+++ typings/algoliasearch-helper.d.ts
@@ -0,0 +1,6 @@
+// TODO: avoid those file, write proper types
+declare module 'algoliasearch-helper' {
+  const value: any;
+
+  export default value;
+}
---
+++ typings/algoliasearch.d.ts
@@ -0,0 +1,6 @@
+// TODO: avoid those file, write proper types
+declare module 'algoliasearch/lite' {
+  const value: any;
+
+  export default value;
+}
