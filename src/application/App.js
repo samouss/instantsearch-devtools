d119d47e92277400c1a28e900a53fc407f33084f
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-02-19 15:08:22 +0100

    refactor(App): use store from the componnet instance

--- src/application/App.js
+++ src/application/App.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import VisibleEventList from './containers/VisibleEventList';
 import VisibleEventDetail from './containers/VisibleEventDetail';
 import Layout from './components/Layout';
 import './index.css';
 
-const App = ({ store }) => (
-  <Provider store={store}>
-    <Layout>
-      <VisibleEventList />
-      <VisibleEventDetail />
-    </Layout>
-  </Provider>
-);
+class App extends Component {
+  constructor() {
+    super();
+
+    this.store = configureStore();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.event) {
+      // @TODO: create an action from the event
+      // don't rely on HookEvent convert them
+      // in application specific "domain"
+      this.store.dispatch(nextProps.event);
+    }
+  }
+
+  render() {
+    return (
+      <Provider store={this.store}>
+        <Layout>
+          <VisibleEventList />
+          <VisibleEventDetail />
+        </Layout>
+      </Provider>
+    );
+  }
+}
 
 App.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
-  store: PropTypes.object.isRequired,
+  event: PropTypes.object,
+};
+
+App.defaultProps = {
+  event: null,
 };
 
 export default App;
